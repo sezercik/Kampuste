af331b813de174ac5001252c381e1eb290af6610
Tolga Mahramanlıoğlu <tolga.mahramanlioglu@gmail.com>
2024-06-13 02:44:45 +0300

    Components dosyaları eklendi

---
+++ src/Kampuste.Maui/wwwroot/js/a.js
@@ -0,0 +1,16 @@
+﻿// wwwroot/js/scrollToBottom.js
+function adjustContentHeight() {
+    const content = document.querySelector('.content');
+    if (content) {
+        const footerHeight = document.querySelector('.footer').offsetHeight;
+        const windowHeight = window.innerHeight;
+        content.style.minHeight = (windowHeight - footerHeight) + 'px';
+    }
+}
+
+window.addEventListener('resize', adjustContentHeight);
+window.addEventListener('orientationchange', adjustContentHeight);
+
+document.addEventListener('DOMContentLoaded', function () {
+    adjustContentHeight();
+});
