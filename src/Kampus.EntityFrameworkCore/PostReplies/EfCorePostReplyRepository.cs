6e618400052f06719dabce2a90d5eadacd355435
sezer <szrakoraler@gmail.com>
2024-06-28 20:45:19 +0300

    efcore repository added

---
+++ src/Kampus.EntityFrameworkCore/PostReplies/EfCorePostReplyRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Kampus.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+
+namespace Kampus.PostReplies
+{
+    public class EfCorePostReplyRepository : EfCoreRepository<KampusDbContext, PostReply, Guid>, IPostReplyRepository
+    {
+        public EfCorePostReplyRepository(IDbContextProvider<KampusDbContext> dbContextProvider) : base(dbContextProvider)
+        {
+        }
+
+        public async Task<List<PostReply>> GetAllReplyOfPost(Guid repliedPostId, int skipCount, int maxResultCount, string sorting, string filter = null)
+        {
+            var dbSet = await GetDbSetAsync();
+            return await dbSet.WhereIf
+                (
+                    !filter.IsNullOrWhiteSpace(),
+                    p => p.Content.Contains(filter)
+                )
+                .OrderBy(sorting)
+                .Skip(skipCount)
+                .Take(maxResultCount)
+                .ToListAsync();
+
+        }
+
+        public async Task<PostReply> GetByIdAsync(Guid replyId)
+        {
+            var dbSet = await GetDbSetAsync();
+            return await dbSet.FirstOrDefaultAsync(p => p.Id == replyId);
+        }
+    }
+}
