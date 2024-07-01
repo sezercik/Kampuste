46da6c8e885341da68db742a6237e00f3a4d30a1
sezer <szrakoraler@gmail.com>
2024-07-01 22:39:47 +0300

    fix list get parameters of userfollows

--- src/Kampus.EntityFrameworkCore/UserFollows/EfCoreUserFollowRepository.cs
+++ src/Kampus.EntityFrameworkCore/UserFollows/EfCoreUserFollowRepository.cs
@@ -19,19 +19,39 @@ namespace Kampus.UserFollows
         public async Task<List<UserFollow>> GetPendingUserFollowers(Guid userId, int skipCount, int maxResultCount, string sorting, string filter = null)
         {
             var dbSet = await GetDbSetAsync();
-            return await dbSet.Where(uf => uf.FolloweeId == userId && uf.Status == UserFolloweeType.Pending && !uf.IsDeleted).ToListAsync();
+            return await dbSet
+                .WhereIf(
+                !filter.IsNullOrWhiteSpace(),
+                uf => uf.Followee.NormalizedUserName.Contains(filter))
+                .Where(uf => uf.FolloweeId == userId && uf.Status == UserFolloweeType.Pending && !uf.IsDeleted)
+                .OrderBy(sorting)
+                .Skip(skipCount)
+                .Take(maxResultCount)
+                .ToListAsync();
         }
 
         public async Task<List<UserFollow>> GetUserFollowees(Guid userId, int skipCount, int maxResultCount, string sorting, string filter = null)
         {
             var dbSet = await GetDbSetAsync();
-            return await dbSet.Where(uf => uf.FollowerId == userId && !uf.IsDeleted).ToListAsync();
+            return await dbSet
+                .WhereIf(!filter.IsNullOrWhiteSpace(), uf => uf.Follower.NormalizedUserName.Contains(filter))
+                .Where(uf => uf.FollowerId == userId && !uf.IsDeleted)
+                .OrderBy(sorting)
+                .Skip(skipCount)
+                .Take(maxResultCount)
+                .ToListAsync();
         }
 
         public async Task<List<UserFollow>> GetUserFollowers(Guid userId, int skipCount, int maxResultCount, string sorting, string filter = null)
         {
             var dbSet = await GetDbSetAsync();
-            return await dbSet.Where(uf => uf.FolloweeId == userId && !uf.IsDeleted).ToListAsync();
+            return await dbSet
+                .WhereIf(!filter.IsNullOrWhiteSpace(), uf => uf.Followee.NormalizedUserName.Contains(filter))
+                .Where(uf => uf.FolloweeId == userId && !uf.IsDeleted)
+                .OrderBy(sorting)
+                .Skip(skipCount)
+                .Take(maxResultCount)
+                .ToListAsync();
         }
     }
 }
