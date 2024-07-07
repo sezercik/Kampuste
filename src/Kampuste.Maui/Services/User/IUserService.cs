8e4b61f82c16bfda338920accc75ed4f681ee04a
Tolga Mahramanlıoğlu <tolga.mahramanlioglu@gmail.com>
2024-07-08 00:36:30 +0300

    users servise guncellendi

---
+++ src/Kampuste.Maui/Services/User/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kampuste.Maui.Services.User
+{
+    public interface IUserService
+    {
+        Task<List<CurrentUsersDetailsDto>> GetPostsAsync();
+        Task<CurrentUsersDetailsDto> GetPostByIdAsync(Guid UserId);
+        Task<List<CurrentUsersDetailsDto>> GetPostListByUserIDAsync(Guid userId, string filter, string sorting, int skipCount, int MaxResultCount);
+        Task<CurrentUsersDetailsDto> PostPostAsync(CurrentUsersDetailsDto User);
+        Task<CurrentUsersDetailsDto> DeletePostAsync(Guid UserId);
+    }
+}
